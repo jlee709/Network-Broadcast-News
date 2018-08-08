905c1c02d0edd30e366fbe896c2d98c12afd2b00
Justin Lee <leejwg.3@gmail.com>
2018-08-09 08:39:00 +0900

    udpated

--- client.js
+++ client.js
@@ -1,48 +1,21 @@
-// jshint esversion:6
-// net library 
-// "create server" will be used in server.js
-// "create connection" method (used in client.js)
+const net = require("net");
 
-// `client.js`
+const client = net.connect(
+  6969,
+  "0.0.0.0",
+  () => {
+    // writes msg to server
+    process.stdin.on("data", chunk => {
+      client.write(chunk);
+    });
 
-// 1. Create a new **net.Socket** and connect to your running socket server.
+    // receives msg from server
+    client.on("data", chunk => {
+      console.log(chunk.toString());
+    });
+  }
+);
 
-// 2. Once connected, pipe your terminal's standard input stream 
-//    to write to your connected socket.
-
-// 3. Whenever the connected socket (client) emits a 'data' event,
-//    then data is being broadcasted from the server, pipe that data out to your 
-//    terminal's standard output stream.
-
-
-const net = require('net');
-const PORT = process.env.PORT || 6969;
-
-
-const client = net.createConnection(PORT, () => {
-  //'connect' listener
-
-  console.log('connected to server!');
-  process.stdin.pipe(client);
-  client.pipe(process.stdout);
+client.on("error", err => {
+  console.log(err);
 });
-
-
-// process.stdin.setEncoding('utf8');
-
-
-// process.stdin.on('data', data => {
-//   client.write(data);
-// });
-
-process.stdin.on('end', () => {
-  process.stdout.write('end');
-});
-
-// client.on('data', (data) => {
-//   console.log(data.toString());
-// });
-
-// client.on('end', () => {
-//   console.log('disconnected from server');
-// });
--- server.js
+++ server.js
@@ -1,86 +1,200 @@
+const net = require("net");
+//Data Arrays
+const clientArr = [];
+const fullChatLog = [];
+const nameChangeLog = [];
+const privateMsgLog = [];
+const serverMsgLog = [];
 
-// net library 
-// "create server" will be used in server.js
-// "create connection" method (used in client.js)
+//User Tools Key
+const specialKeys = `
+  ? : Help
+  $ : Set Username:  '@sample'
+  / : Private Message to Admin : '/msg'
+  @ : Private Message: '@User + Msg'
+  ~ : See User List
+`;
+//Admin Tools Key
+const adminTools = `
+  ? : Help
+  ! : Kick User: '!sampleUser'
+  @ : Private Message: '@User Msg'
+  ~ : See User List
+  * : Read Private Message Log
+  $ : Read Name Change Log
+  % : Read Chat Log
+  # : Read Direct Server Message Log
+`;
+//Custom Message Divider
+const divider = "-----------------";
 
-// `server.js`. use net library 
+const server = net.createServer(connection => {
+  //Server Admin
+  server.userName = "[ADMIN]";
+  //When socket first connects, give anonymous userName/send user Tools
+  clientArr.push(connection);
+  connection.userName = `Anonymous${clientArr.length}`;
+  connection.msgTimer = [];
 
-//1. Use the [**net**](https://nodejs.org/api/net.html) module to create a new server 
-// that listens on a specified address 
-//   "`0.0.0.0` and port `6969` and listens for and accepts socket connections."
+  connection.write(`${specialKeys}\nENJOY CHATTING!\n${divider}`);
+  //Event Listener for when data comes in. Checks for character for special tools
+  connection.on("data", data => {
+    switch (data.toString().charAt(0)) {
+      case "?": //Help - Send User Tools
+        connection.write(`${specialKeys}`);
+        break;
 
-//2. Manage which sockets are connected, and maintain your sockets so that it removes any 
-//   sockets that disconnect from your server.
+      case "$": //User request to change name
+        if (
+          clientArr.some(
+            element =>
+              element.userName === data.toString().slice(1, -1) ||
+              data
+                .toString()
+                .slice(1, -1)
+                .toLowerCase()
+                .indexOf("admin") > 0
+          )
+        ) {
+          connection.write("Username Invalid or In Use!");
+        } else {
+          clientArr.forEach(element => {
+            if (element === connection) {
+              nameChangeLog.push(`${connection.userName}: ${data.toString()}`);
+              connection.userName = data.toString().slice(1, -1);
+            }
+          });
+        }
+        break;
 
-//3. Each connected socket is a **Duplex** stream, when it emits a 'data' event, 
-//   broadcast the data to all sockets.
+      case "/": //User sends private message to admin
+        console.log(
+          `(${connection.userName}) (${data.toString().slice(1, -1)})`
+        );
+        serverMsgLog.push(`${connection.userName}: ${data.toString()}`);
+        break;
 
+      case "@": //Private Message
+        let privateUser = data
+          .toString()
+          .slice(1, data.toString().indexOf(" "));
+        let message = data
+          .toString()
+          .slice(data.toString().indexOf(" "))
+          .slice(1);
+        clientArr.forEach(element => {
+          if (element.userName === privateUser) {
+            element.write(
+              `**Direct Message from ${connection.userName}: ${message}`
+            );
+          }
+        });
+        privateMsgLog.push(`${connection.userName}: ${data.toString()}`);
+        break;
 
-//  use nodemon = npm install -g nodemon
-// create server create connection very important most research there due to callbacks etc. TCP
+      case "~": //Request to see user list
+        connection.write(
+          `${clientArr.map(element => element.userName).join(", ")}`
+        );
+        break;
 
-console.log('how to test server here'); 
-
-const net = require('net');
-const PORT = process.env.PORT || 6969;
-const clients = [];
-const admin = '[ADMIN]';
-
-
-// fires msg from sender to the clients 
-const broadcast = (sender, message) => clients
-.filter(c => c !== sender)
-.forEach(c => {
-  console.log(c);
-  c.write(message);
-});
-
-
-
-const server = net.createServer((client) => {
-  // registerclient into cliemts
-  console.log('client conntected');
-  clients.push(client);
-  client.username = null;
-  // prompt user
-  client.write("what is your user name? \n");
-  client.on('data', (data) => {
-    if(client.username === null){
-      // set username to data 
-      client.username = data.toString();
-      client.write(`Welcome ${client.username}`);
-    }else{
-      broadcast(client, data.toString());
+      default:
+        //Default user sends message to group
+        clientArr.forEach(element => {
+          if (element !== connection) {
+            element.write(
+              `${connection.userName}: ${data
+                .toString()
+                .slice(0, -1)}\n${divider}`
+            );
+            fullChatLog.push(`${connection.userName}: ${data.toString()}`);
+          } else {
+            element.write(`${divider}`);
+          }
+        });
+    }
+    //spam filter start
+    connection.msgTimer.push({
+      time: new Date().getTime() //Creates object that records time of message
+    });
+    //make sure at least 5 messages have been sent
+    if (connection.msgTimer.length >= 8) {
+      //check if last message came within 5 seconds of the 5th message from end
+      if (
+        connection.msgTimer[connection.msgTimer.length - 1].time -
+          connection.msgTimer[connection.msgTimer.length - 8].time <=
+        8000
+      ) {
+        connection.end("GTFO! KICKED FOR SPAMMING! >;(");
+      }
     }
   });
 });
 
+server.listen(
+  {
+    port: 6969
+  },
+  () => {
+    console.log(`Connection is ON\n${adminTools}`);
+  }
+);
 
-server.listen(PORT, () => {
-  console.log(`listening to ${PORT}`);
+//Server Admin chats and admin tools
+process.stdin.on("data", data => {
+  switch (data.toString().charAt(0)) {
+    case "?": //Display admin tools
+      // console.log(adminTools);
+      break;
+
+    case "!": //Kick User
+      clientArr
+        .filter(element => {
+          return element.userName === data.toString().slice(1, -1);
+        })
+        .forEach(element => {
+          clientArr.splice(clientArr.indexOf(element, 1));
+          element.end(`${server.userName}: You Have Been Kicked`);
+        });
+      break;
+
+    case "@": //Send private Message to user
+      let privateUser = data.toString().slice(1, data.toString().indexOf(" "));
+      let message = data
+        .toString()
+        .slice(data.toString().indexOf(" "))
+        .slice(1);
+      clientArr.forEach(element => {
+        if (element.userName === privateUser) {
+          element.write(`***${server.userName} - PRIVATE - : ${message}`);
+        }
+      });
+      break;
+
+    case "~": //Request to see user list
+      console.log(`${clientArr.map(element => element.userName).join(", ")}`);
+      break;
+
+    case "*": //Read Private Message Log
+      console.log(privateMsgLog.join(""));
+      break;
+
+    case "$": //Read name change log
+      console.log(nameChangeLog.join(""));
+      break;
+
+    case "%": //read chat log
+      console.log(fullChatLog.join(""));
+      break;
+
+    case "#": // read user to admin message log
+      console.log(serverMsgLog.join(""));
+      break;
+
+    default:
+      //Broadcast Message to all
+      clientArr.forEach(element => {
+        element.write(`${server.userName} : ${data}`);
+      });
+  }
 });
-
-
-
-
-
-
-
-
-// tubeTut
-
-// var http = require('http');
-// function onRequest(request, response){
-//   console.log("User made a request" + request.url);
-//   response.writeHead(200, {"Context-Type": 'text/plain'});
-//   response.write("here is some data");
-//   response.end();
-// }
-
-// http.createServer(onRequest).listen(6969);
-
-
-
-
-
-//old code 
